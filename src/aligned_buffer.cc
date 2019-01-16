a3b4cf209ee6f711643870ccfd253c71052682d7
Hugo Lindström <hugo.lindstrom@trimble.se>
2019-01-16 16:34:01 +0100

    Add NULL to indicate that clang-tidy is indeed invoked

--- src/aligned_buffer.cc
+++ src/aligned_buffer.cc
@@ -4,5 +4,6 @@ using TU8 = uint8_t;
 
 bool Compare(size_t from, const void* buf_ptr, size_t length) {
   const TU8* comp_ptr = static_cast<const TU8*>(buf_ptr);
+  const TU8* t = NULL;
   return false;
 }
